2d709a492852e5b53cd5d5d0418a2f1da555ce08
Yaschenko <yura.yaschenko@gmail.com>
2018-09-25 12:19:15 +0300

    move template folder from contracts

---
+++ template/BasicContract.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.23;
+
+
+import "./BasicContractPrivate.sol";
+
+contract BasicContract  is BasicContractPrivate {
+
+    address[] private admins;
+    address[] private decliners;
+
+    mapping (uint => uint) private confirmationStatus;
+    uint minimumConfirmationsCount = {{ minimum_confirmations_count }};
+
+    uint confirmationsCount = 0;
+    
+    constructor() public payable {
+        setPaymentInfo();
+    }
+
+    function setAdmins() private {
+        {% for admin in admins_list %}
+        admins.push({{ admin }});
+        {% endfor %}
+    }
+    function setDecliners() private {
+        {% for decliner in decliners_list %}
+        decliners.push({{ decliner }});
+        {% endfor %}
+    }
+    function getAdmins() public view returns(address[] memory) {
+        return admins;
+    }
+    function getDecliners() public view returns(address[] memory) {
+        return decliners;
+    }
+
+    function getContractRules() public view returns(string) {
+        return rule;
+    }
+
+    function setPaymentInfo() internal {
+        //set list of users who will get ethers
+        //when contract will confirme
+        {% for payment in payments_list %}
+        payToUsers.push({{ payment.user }});
+        payToUsersAmount.push({{ payment.amount }} ether);
+        {% endfor %}
+    }
+
+    function getPaymentInfo() public view returns(address[] memory, uint[] memory) {
+        return (payToUsers, payToUsersAmount);
+    }
+
+    function sendConfirmation() public {
+        for (uint i = 0; i < admins.length; i++) {
+            if (admins[i] != msg.sender) {
+                continue;
+            }
+            //checking if this user have already confiremed this contract
+            require(confirmationStatus[i] == 0);
+            
+            confirmationStatus[i] = 1;
+            confirmationsCount++;
+            
+            //
+            WizardManager(managerAddress).userConfirmedContract(msg.sender);
+            //when contract get minimum confirmations it will be confirmed
+            if (confirmationsCount >= minimumConfirmationsCount) {
+                confirmContract();
+            }
+        }
+    }
+
+    function cancel() public {
+        for (uint i = 0; i < decliners.length; i++) {
+            if (decliners[i] == msg.sender) {
+                WizardManager(managerAddress).contractCanceled();
+                selfdestruct(owner);
+            }
+        }
+    }
+}
---
+++ template/README.md
@@ -0,0 +1,19 @@
+# Templates variables
+
+**manager_address** - address
+
+**service_fee** - float
+
+**rule** - string(json)
+
+**type** - string
+
+**minimum_price** - float
+
+**minimum_confirmations_count** - int
+
+**admins_list** - array of addresses
+
+**decliners_list** - array of addresses
+
+**payments_list** - array of structure. Fields: user, amount
